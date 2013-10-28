9231ea1cb25101bba015dc47ac58ef54af76fa36
jackhanford <jackhanford@gmail.com>
2013-10-27 19:53:18 -0700

    small changes, groundwork for slideshow

--- js/main.js
+++ js/main.js
@@ -6,4 +6,25 @@ $(document).ready(function() {
     $(".toppadd").hide();
     // $(".profilePic").hide();
   });
+
+	function slideSwitch() {
+    	var $active = $('#slideshow img.active');
+
+    	if ( $active.length == 0 ) $active = $('#slideshow img:last');
+
+    	var $next =  $active.next().length ? $active.next()
+        	: $('#slideshow img:first');
+
+    	$active.addClass('last-active');
+        
+    	$next.css({opacity: 0.0})
+        	.addClass('active')
+        	.animate({opacity: 1.0}, 1000, function() {
+            	$active.removeClass('active last-active');
+        	});
+		}
+
+		$(function() {
+    		setInterval( "slideSwitch()", 1000 );
+	});
 });
