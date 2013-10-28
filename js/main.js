ba8ce4604d45ae55b3242a0fa5dcdaade69f7ceb
jackhanford <jackhanford@gmail.com>
2013-10-28 14:38:42 -0700

    added a lot of new things

--- js/main.js
+++ js/main.js
@@ -4,27 +4,6 @@ $(document).ready(function() {
     $(".about.none").fadeIn();
     $(this).hide();
     $(".toppadd").hide();
-    // $(".profilePic").hide();
   });
-
-	function slideSwitch() {
-    	var $active = $('#slideshow img.active');
-
-    	if ( $active.length == 0 ) $active = $('#slideshow img:last');
-
-    	var $next =  $active.next().length ? $active.next()
-        	: $('#slideshow img:first');
-
-    	$active.addClass('last-active');
-        
-    	$next.css({opacity: 0.0})
-        	.addClass('active')
-        	.animate({opacity: 1.0}, 1000, function() {
-            	$active.removeClass('active last-active');
-        	});
-		}
-
-		$(function() {
-    		setInterval( "slideSwitch()", 1000 );
-	});
 });
+
