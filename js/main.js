c4feecc3f6fe23a223e49ee7d3732c7eb9a257d9
jackhanford <jackhanford@gmail.com>
2013-11-06 18:37:34 -0800

    smoother ux

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   	e.preventDefault();
     $(".about.none").toggle().addClass('slideleft');
     $(this).hide();
-    $(".toppadd").toggle();
+    $(".toppadd").slideUp();
   });
 $(document).ready(function() {
   $('.tip').tipr();
