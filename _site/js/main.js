ef583baa8c8cda3cf34fc75f9561add362800389
Jack Hanford <jackhanford@gmail.com>
2013-12-11 12:08:38 -0800

    Starting to use jekyll

---
+++ _site/js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+  $('.tip').tipr();
+  $("#jbtn").click(function(e) {
+  	e.preventDefault();
+    $(".about.none").fadeIn();
+    $(this).fadeOut("fast")
+    $(".toppadd").slideUp();
+  });
+
+  $(document).ready(function() {
+    $('.slideshow img:gt(0)').fadeOut();
+      setInterval(function(){
+        $('.slideshow :first-child').fadeIn()
+          .next('img').fadeOut()
+          .end().appendTo('.slideshow');},
+        3000);
+    });
+
+  $('#contact').click(function() {
+    $('#contactInfo').slideDown(function() {
+      $('#contact').slideUp('fast');
+    });
+  });
+
+
+  $('.resumetoggle').click(function() {
+    $('.resume').toggle();
+  });
+
+
+});
